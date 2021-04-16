38bcaaac297477a1701b55c9147397d1d655265e
Atj Rupesh <62754636+atjrupesh@users.noreply.github.com>
2021-04-16 12:33:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ weight = float(input("enter your weight in kg: "))
 BMI = (round(weight / height ** 2)) 
 bmi_2 = print(round(BMI,2))
 
-if BMI  <= 100:
+if BMI  <= 150:
   if BMI < 18.5:
     print(f"Your BMI IS {BMI} you are Under weight")
   elif BMI < 25:
